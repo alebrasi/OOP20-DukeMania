c4997c373dbef0b776106d2736660ab9c76c65db
alebrasi <alessandro.brasini1@gmail.com>
2021-07-26 19:31:45 +0200

    Fixed resizing problems

--- core/src/it/dukemania/DukeMania.java
+++ core/src/it/dukemania/DukeMania.java
@@ -2,11 +2,8 @@ package it.dukemania;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.*;
@@ -16,28 +13,36 @@ import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class DukeMania extends ApplicationAdapter {
-	SpriteBatch batch;
-	FreeTypeFontGenerator generator;
-	Stage stage;
-	Label.LabelStyle lblStyle;
-	TextButton.TextButtonStyle txtBtnStyle;
-
+	private FreeTypeFontGenerator generator;
+	private Stage stage;
+	private Stage backStage;
+	private Label.LabelStyle lblStyle;
+	private Image backgroundImage;
+	private TextButton.TextButtonStyle txtBtnStyle;
+	private Skin skin;
+	private Camera camera;
+	
 	@Override
 	public void create () {
 		String TEXTURE_PATH = "textures/quantum-horizon/quantum-horizon-ui.json";
 		String MENU_BACKGROUND_IMAGE_PATH = "DukeMania.png";
 		String FONT_PATH = "fonts/agency-fb.ttf";
 
+		Gdx.graphics.setFullscreenMode(Gdx.graphics.getDisplayMode());
+
 		int fontSize = 50;
-		int fontBorderWidth = 3;
+		float fontBorderWidth = 0.5f;
 		Color fontColor = Color.BLACK;
 		float buttonPadding = 20f;
 
-		batch = new SpriteBatch();
-		stage = new Stage(new StretchViewport(1920, 1080));
-
 		Texture img = new Texture(MENU_BACKGROUND_IMAGE_PATH);
-		Image backgroundImage = new Image(img);
+		backgroundImage = new Image(img);
+
+		camera = new OrthographicCamera(1920, 1080);
+		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
+
+		stage = new Stage(new ExtendViewport(camera.viewportWidth, camera.viewportHeight, camera));
+		backStage = new Stage(new StretchViewport(backgroundImage.getWidth(), backgroundImage.getHeight()));
 
 		float screenWidth = stage.getWidth();
 		float screenHeight = stage.getHeight();
@@ -48,16 +53,30 @@ public class DukeMania extends ApplicationAdapter {
 		parameter.borderWidth = fontBorderWidth;
 		parameter.color = fontColor;
 		BitmapFont menuFont = generator.generateFont(parameter);
+		menuFont.getRegion().getTexture().setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
 
-		Skin skin = new Skin(Gdx.files.internal(TEXTURE_PATH));
+		skin = new Skin();
+		skin.add("font", menuFont);
 		skin.add("title", menuFont);
+		skin.addRegions(new TextureAtlas(Gdx.files.internal("textures/quantum-horizon/quantum-horizon-ui.atlas")));
+		skin.load(Gdx.files.internal(TEXTURE_PATH));
 
+		/*
 		txtBtnStyle = new TextButton.TextButtonStyle();
 		lblStyle = new Label.LabelStyle();
+		*/
 
 		TextButton btnPlay = new TextButton("PLAY", skin);
 		TextButton btnQuit = new TextButton("QUIT", skin);
 		TextButton btnOptions = new TextButton("OPTIONS", skin);
+
+		btnOptions.pack();
+		btnPlay.pack();
+		btnQuit.pack();
+
+		btnPlay.setTransform(true);
+		btnQuit.setTransform(true);
+		btnOptions.setTransform(true);
 
 		btnPlay.addListener(new ClickListener() {
 			@Override
@@ -77,6 +96,9 @@ public class DukeMania extends ApplicationAdapter {
 			@Override
 			public void clicked(InputEvent event, float x, float y) {
 				System.out.println("Quit");
+				Gdx.app.exit();
+				dispose();
+				System.exit(0);
 			}
 		});
 
@@ -104,7 +126,7 @@ public class DukeMania extends ApplicationAdapter {
 		mainMenuContainer.setActor(table);
 		mainMenuContainer.setPosition(screenWidth / 2, (screenHeight / 2) - byPercent(screenHeight, 0.12f));
 
-		stage.addActor(backgroundImage);
+		backStage.addActor(backgroundImage);
 		stage.addActor(mainMenuContainer);
 		Gdx.input.setInputProcessor(stage);
 	}
@@ -115,25 +137,30 @@ public class DukeMania extends ApplicationAdapter {
 
 	@Override
 	public void resize(int width, int height) {
+		System.out.println("Resize called " + width + " " + height);
+		skin.getFont("title").getData().setScale(width * 0.002f, height * 0.002f);
 		stage.getViewport().update(width, height);
+		backStage.getViewport().update(width, height);
+		backStage.getViewport().apply();
+		stage.getViewport().apply();
 	}
 
 	@Override
 	public void render () {
 		//Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
+		backStage.act();
+		stage.act();
 
-		//batch.draw(img, 0, 0);
+		backStage.draw();
 		stage.draw();
-		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
 		stage.dispose();
 		generator.dispose();
+		System.out.println("Disposed");
 		//img.dispose();
 	}
 }
