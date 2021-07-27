f76a5ba2e479c1b91942876d9dce905572abaea4
alebrasi <alessandro.brasini1@gmail.com>
2021-07-27 22:39:30 +0200

    Added Track class for testing purpose

---
+++ core/src/it/dukemania/View/menu/Track.java
@@ -0,0 +1,12 @@
+package it.dukemania.View.menu;
+
+public class Track {
+
+    public String trackName;
+    public String instrumentName;
+
+    public Track(String trackName, String instrumentName) {
+        this.trackName = trackName;
+        this.instrumentName = instrumentName;
+    }
+}
