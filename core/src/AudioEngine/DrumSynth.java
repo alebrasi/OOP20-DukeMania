45155d43a5b9557c2434450740d928d3fe95e8e1
uwuflanny <gianlumiglia@gmail.com>
2021-07-31 11:32:23 +0200

    added drum kick, drum test

--- core/src/AudioEngine/DrumSynth.java
+++ core/src/AudioEngine/DrumSynth.java
@@ -3,6 +3,7 @@ package AudioEngine;
 import sun.jvm.hotspot.debugger.remote.amd64.RemoteAMD64Thread;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.logging.Filter;
 import java.util.stream.DoubleStream;
 import java.util.stream.IntStream;
@@ -48,7 +49,7 @@ public class DrumSynth implements Synth{
                 buff[i] = rnd.nextFloat();
             }
             return new DrumIterator<Float>() {
-                private final EnveloperIterator<Float> env = new Enveloper(10l, 1f, 100l).createEnveloper();
+                private final EnveloperIterator<Float> env = new Enveloper(10l, 1f, 25l).createEnveloper();
                 private int passed = 0;
                 @Override
                 public boolean hasNext() {
@@ -66,21 +67,57 @@ public class DrumSynth implements Synth{
             };
         }
 
+        static DrumIterator<Float> getKick(){
+            Random rnd = new Random();
+
+            double[] steps = {(Settings.WAVETABLE_SIZE * (220)) / Settings.SAMPLE_RATE, (Settings.WAVETABLE_SIZE * (55)) / Settings.SAMPLE_RATE};
+            Function<Long,Float> lfoOsc1 = LFOFactory.straightLineLFO(0.1f, 80);
+            double [] pos = new double[2];
+            float[] snarebuff = new float[300000];
+            boolean lfoEnded = false;
+            float noteLfoVal = 0f;
+            for(int i = 0;i<300000;i++){
+                noteLfoVal = lfoEnded ? noteLfoVal : lfoOsc1.apply((long) i);
+                if(noteLfoVal <= 0.11){
+                    lfoEnded = true;
+                    noteLfoVal = 0;
+                }
+                snarebuff[i] =  WaveTable.Square.getAt((int)((pos[0] = pos[0] + steps[0] * noteLfoVal) % Settings.WAVETABLE_SIZE)) +
+                        WaveTable.Sine.getAt((int)((pos[1] = pos[1] + steps[1]) % Settings.WAVETABLE_SIZE)) ;
+            }
+            return new DrumIterator<Float>() {
+                private final EnveloperIterator<Float> env = new Enveloper(10l, 1f, 100l).createEnveloper();
+                private int passed = 0;
+                @Override
+                public boolean hasNext() {
+                    return env.hasNext();
+                }
+                @Override
+                public Float next() {
+                    return snarebuff[passed++] * env.next();
+                }
+                @Override
+                public void refresh() {
+                    this.passed = 0;
+                    env.refresh(10);
+                }
+            };
+        }
+
         static {
             Snare.sampleBuffer = getSnare();
             Hat.sampleBuffer = getHat();
-
-
-
-
+            Kick.sampleBuffer = getKick();
         }
     }
 
 
-    private final Set<DrumSamples> active = new HashSet<>();
+    private final List<DrumSamples> active = new ArrayList<>();
 
     public DrumSynth(){
+        active.add(DrumSamples.Kick);
         active.add(DrumSamples.Snare);
+        active.add(DrumSamples.Hat);
     }
 
     @Override
@@ -93,8 +130,8 @@ public class DrumSynth implements Synth{
         return (float) active.stream().filter(x->x.sampleBuffer.hasNext()).mapToDouble(x->x.sampleBuffer.next()).sum();
     }
 
-    public void test(){
-        active.stream().findAny().get().sampleBuffer.refresh();
+    public void test(int pos){
+        active.get(pos).sampleBuffer.refresh();
     }
 
 }
--- core/src/AudioEngine/Engine.java
+++ core/src/AudioEngine/Engine.java
@@ -16,7 +16,7 @@ public class Engine {
     public Engine(){
 
         SynthBuilderImpl b = new SynthBuilderImpl();
-        b.setEnveloper(new Enveloper(10l, 1f, 1000l));
+        b.setEnveloper(new Enveloper(10l, 1f, 100l));
         b.setWavetables(new WaveTable[]{WaveTable.Sine});
         b.setOffsets(new double[]{1d});
 
@@ -39,23 +39,35 @@ public class Engine {
      */
     public void playBuffer(){
 
+        /*
         int num = synthetizers.stream().mapToInt(Synth::checkKeys).sum();
         for(int i=0;i<buffer.length;i++){
             buffer[i] = (float) (synthetizers.stream().mapToDouble(Synth::getSample).sum() * 1);
         }
-
-
-
-        if(pos++ % 50 == 0){
-            synthetizers.get(0).playTimedNote(100f, 500000l);
+        if(pos++ % 10 == 0){
+            synthetizers.get(0).playTimedNote(100f, 5l);
         }
-/*
-        int num = ds.checkKeys();
+        */
+
+        if(pos == 0 || pos == 75 ){
+            ds.test(0);
+        }
+        if(pos == 15 || pos == 45 || pos == 60 || pos == 52 || pos == 67 || pos == 105){
+            ds.test(2);
+        }
+        if(pos == 30 || pos == 90){
+            ds.test(1);
+        }
+
+
+        pos = (pos +1) % 120;
+
+        //int num = ds.checkKeys();
         for(int i=0;i<buffer.length;i++){
             buffer[i] = ds.getSample();
         }
 
-*/
+
 
         ad.writeSamples(this.buffer, 0, buffer.length);
     }
