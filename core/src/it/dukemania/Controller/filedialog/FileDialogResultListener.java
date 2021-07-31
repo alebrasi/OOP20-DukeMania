55365ad889dce10f622ea9cc227234b5be94ea79
alebrasi <alessandro.brasini1@gmail.com>
2021-07-31 11:26:14 +0200

    Removed enum

--- core/src/it/dukemania/Controller/filedialog/FileDialogResultListener.java
+++ core/src/it/dukemania/Controller/filedialog/FileDialogResultListener.java
@@ -1,9 +1,5 @@
 package it.dukemania.Controller.filedialog;
 
-enum DialogResult {
-    OK, CANCEL
-}
-
 public interface FileDialogResultListener {
     void result(DialogResult res, String fileName, String filePath);
 }
