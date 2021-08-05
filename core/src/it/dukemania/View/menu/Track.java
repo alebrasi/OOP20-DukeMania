cb10c8e1ef55a2631bd7da6bec2e61fb0b85bed2
alebrasi <alessandro.brasini1@gmail.com>
2021-08-05 18:32:45 +0200

    Field encapsulation

--- core/src/it/dukemania/View/menu/Track.java
+++ core/src/it/dukemania/View/menu/Track.java
@@ -2,11 +2,26 @@ package it.dukemania.View.menu;
 
 public class Track {
 
-    public String trackName;
-    public String instrumentName;
+    final private String trackName;
+    final private String instrumentName;
+    final private int trackID;
 
-    public Track(String trackName, String instrumentName) {
+    public Track(final int trackID, final String trackName, final String instrumentName) {
         this.trackName = trackName;
         this.instrumentName = instrumentName;
+        this.trackID = trackID;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public String getInstrumentName() {
+        return instrumentName;
+    }
+
+
+    public int getTrackID() {
+        return trackID;
     }
 }
