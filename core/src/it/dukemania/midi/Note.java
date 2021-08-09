f7e2e1f5c545c82d00ec9f066f679a668f3e1540
laura <laura.leonardi9@studio.unibo.it>
2021-08-09 16:22:09 +0200

    add principal classes for parsing

---
+++ core/src/it/dukemania/midi/Note.java
@@ -0,0 +1,29 @@
+package it.dukemania.midi;
+
+import java.util.Optional;
+
+public abstract class Note {
+    private final Optional<Double> duration;
+    private final int startTime;
+    private final int identifier;
+
+
+    public Note(final Optional<Double> duration, final int startTime, final int identifier) {
+        this.duration = duration;
+        this.startTime = startTime;
+        this.identifier = identifier;
+    }
+
+    public final Optional<Double> getDuration() {
+        return duration;
+    }
+
+    public final int getStartTime() {
+        return startTime;
+    }
+
+    public final int getIdentifier() {
+        return identifier;
+    }
+
+}
