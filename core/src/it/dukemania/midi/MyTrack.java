68226967e3d896525680b4dd3b82e2f1d6c4024f
laura <laura.leonardi9@studio.unibo.it>
2021-08-10 13:16:10 +0200

    add noteMaxDuration, add removeNote, Collection -> List

--- core/src/it/dukemania/midi/MyTrack.java
+++ core/src/it/dukemania/midi/MyTrack.java
@@ -1,26 +1,45 @@
 package it.dukemania.midi;
 
-import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 
 public class MyTrack {
 
     private final Enum<InstrumentType> instrument;
-    private final Collection<Note> notes;
+    private final List<Note> notes;
     private final int channel;
+    private final Map<Integer, Double> notesMaxDuration;
 
-    public MyTrack(final Enum<InstrumentType> instrument, final Collection<Note> notes, final int channel) {
+    public MyTrack(final Enum<InstrumentType> instrument, final List<Note> notes, final int channel) {
         this.instrument = instrument;
         this.notes = notes;
         this.channel = channel;
+        this.notesMaxDuration = calcMaxDuration();
     }
+
     public final Enum<InstrumentType> getInstrument() {
         return instrument;
     }
-    public final Collection<Note> getNotes() {
+    public final List<Note> getNotes() {
         return notes;
     }
     public final int getChannel() {
         return channel;
+    }
+
+    public final Map<Integer, Double> getNotesMaxDuration() {
+        return notesMaxDuration;
+    }
+
+    public final void deleteNote(final Note note) {
+        this.notes.remove(note);
+    }
+
+    private Map<Integer, Double> calcMaxDuration() {
+        return notes.stream()
+                    .collect(Collectors.toMap(Note::getIdentifier, n -> n.getDuration().get(), (d1, d2) -> d1 > d2 ? d1 : d2));
     }
 
 }
--- core/src/it/dukemania/midi/Song.java
+++ core/src/it/dukemania/midi/Song.java
@@ -1,19 +1,19 @@
 package it.dukemania.midi;
 
-import java.util.Collection;
+import java.util.List;
 
 public class Song {
-	private final String title;
+    private final String title;
     private final double  duration;
-    private final Collection<MyTrack> tracks;
+    private final List<MyTrack> tracks;
     private final double bpm;                  //potrebbe essere un int, controllare eventuali valori di default
 
-    public Song(final String title, final double duration, final Collection<MyTrack> tracks, final double bPM) {
+    public Song(final String title, final double duration, final List<MyTrack> tracks, final double bPM) {
         super();
         this.title = title;
         this.duration = duration;
         this.tracks = tracks;
-        bpm = bPM;
+        this.bpm = bPM;
     }
     public final String getTitle() {
         return title;
@@ -24,7 +24,7 @@ public class Song {
     public final double getDuration() {
         return duration;
     }
-    public final Collection<MyTrack> getTracks() {
+    public final List<MyTrack> getTracks() {
         return tracks;
     }
 
