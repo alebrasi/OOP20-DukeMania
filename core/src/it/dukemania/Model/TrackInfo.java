1657a2bdd71f45481f6fbbfe9ff7590b1f4faa36
alebrasi <alessandro.brasini1@gmail.com>
2021-08-15 11:03:16 +0200

    Class name refactoring

--- core/src/it/dukemania/Model/TrackInfo.java
+++ core/src/it/dukemania/Model/TrackInfo.java
@@ -1,11 +1,11 @@
 package it.dukemania.Model;
 
-public class Track {
+public class TrackInfo {
     final private String trackName;
     final private String instrumentName;
     final private int trackID;
 
-    public Track(final int trackID, final String trackName, final String instrumentName) {
+    public TrackInfo(final int trackID, final String trackName, final String instrumentName) {
         this.trackName = trackName;
         this.instrumentName = instrumentName;
         this.trackID = trackID;
