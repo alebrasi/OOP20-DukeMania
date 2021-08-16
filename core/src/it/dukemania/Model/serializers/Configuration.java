00d55d5eba555acbda8870bd20945b8c6bf8cdb4
alebrasi <alessandro.brasini1@gmail.com>
2021-08-16 15:45:13 +0200

    Added synthesizer parse utility in configuration class

--- core/src/it/dukemania/Model/serializers/Configuration.java
+++ core/src/it/dukemania/Model/serializers/Configuration.java
@@ -1,9 +1,12 @@
 package it.dukemania.Model.serializers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.dukemania.Model.Song;
+import it.dukemania.Model.serializers.synthesizer.SynthInfo;
+import it.dukemania.audioengine.SynthBuilder;
 import it.dukemania.util.storage.Storage;
 import it.dukemania.util.storage.StorageFactoryImpl;
 
@@ -18,7 +21,6 @@ public class Configuration {
         private final Storage configurationStorage = new StorageFactoryImpl().getConfigurationStorage();
 
         private final String SONGS_CONFIGURATION_PATH = "configs/song_config.json";
-        private final String SYNTHESIZERS_CONFIGURATION_PATH = "configs/synthesizers_config.json";
 
         public List<Song> readAll() throws IOException {
             String json = configurationStorage.readFileAsString(SONGS_CONFIGURATION_PATH);
@@ -33,6 +35,28 @@ public class Configuration {
             String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(songs);
             configurationStorage.createFileIfNotExists(SONGS_CONFIGURATION_PATH);
             configurationStorage.writeStringOnFile(SONGS_CONFIGURATION_PATH, json);
+        }
+    }
+
+    public static class synthesizer {
+
+        private final ObjectMapper mapper = new ObjectMapper();
+        private final Storage configurationStorage = new StorageFactoryImpl().getConfigurationStorage();
+        private final String SYNTHESIZERS_CONFIGURATION_PATH = "configs/synthesizers_config.json";
+
+        public List<SynthInfo> readAll() throws IOException {
+            String json = configurationStorage.readFileAsString(SYNTHESIZERS_CONFIGURATION_PATH);
+
+            JavaType listSynthInfoType = mapper.constructType(new TypeReference<List<SynthInfo>>() {
+            });
+
+            return mapper.readValue(json, listSynthInfoType);
+        }
+
+        public void writeAll(final List<SynthInfo> synths) throws IOException {
+            String json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(synths);
+            configurationStorage.createFileIfNotExists(SYNTHESIZERS_CONFIGURATION_PATH);
+            configurationStorage.writeStringOnFile(SYNTHESIZERS_CONFIGURATION_PATH, json);
         }
     }
 }
