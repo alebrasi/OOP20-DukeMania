0543673dedcbad15b03cd423037f691ff817eb22
alebrasi <alessandro.brasini1@gmail.com>
2021-08-16 23:24:19 +0200

    Started synth deserializer

--- core/src/it/dukemania/Model/serializers/synthesizer/SynthBuilderDeserializer.java
+++ core/src/it/dukemania/Model/serializers/synthesizer/SynthBuilderDeserializer.java
@@ -1,4 +1,27 @@
 package it.dukemania.Model.serializers.synthesizer;
 
-public class SynthBuilderDeserializer {
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import it.dukemania.audioengine.SynthBuilderImpl;
+
+import java.io.IOException;
+
+public class SynthBuilderDeserializer extends StdDeserializer<SynthBuilderImpl> {
+
+    public SynthBuilderDeserializer() {
+        this(null);
+    }
+
+    public SynthBuilderDeserializer(final Class<?> vc) {
+        super(vc);
+    }
+
+    @Override
+    public SynthBuilderImpl deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JsonProcessingException {
+        SynthBuilderImpl synth = new SynthBuilderImpl();
+
+        return synth;
+    }
 }
