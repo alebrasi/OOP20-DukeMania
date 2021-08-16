aef980dee5a17387c615be48c86afb1e91e94720
alebrasi <alessandro.brasini1@gmail.com>
2021-08-16 16:04:24 +0200

    Minor modification

--- desktop/src/it/dukemania/desktop/DesktopLauncher.java
+++ desktop/src/it/dukemania/desktop/DesktopLauncher.java
@@ -2,14 +2,11 @@ package it.dukemania.desktop;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import it.dukemania.DukeManiaTest;
+import it.dukemania.DukeMania;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		//new Lwjgl3Application(new DukeMania(), config);
-		//new Lwjgl3Application(new PlayScreen(), config);
-		new Lwjgl3Application(new DukeManiaTest(), config);
-		//new Lwjgl3Application(new TitleScreen(), config);
+		new Lwjgl3Application(new DukeMania(), config);
 	}
 }
