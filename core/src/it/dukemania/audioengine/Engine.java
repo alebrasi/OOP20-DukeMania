585d0d84515c28735299b002d5fb167d31f2a648
uwuflanny <gianlumiglia@gmail.com>
2021-08-17 14:47:10 +0200

    added volume regulator

--- core/src/it/dukemania/audioengine/Engine.java
+++ core/src/it/dukemania/audioengine/Engine.java
@@ -2,94 +2,91 @@ package it.dukemania.audioengine;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.AudioDevice;
+import it.dukemania.midi.MidiTrack;
+import it.dukemania.midi.TrackImpl;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Engine {
 
-    private final List<KeyboardSynth> synthetizers = new ArrayList<>();
+    public final List<Synth> synthetizers = new ArrayList<>();
     private final AudioDevice ad = Gdx.audio.newAudioDevice((int) Settings.SAMPLE_RATE, true);
     private final float [] buffer = new float[Settings.BUFFER_LENGHT];
-    private final DrumSynth ds = new DrumSynth();
+
+    private float step = 0, vol = 0;
+    private int old = 0;
+    private int att = 0;
 
     public Engine() {
 
-        SynthBuilderImpl b = new SynthBuilderImpl();
-        b.setEnveloper(new Enveloper(10l, 1f, 100l));
-        b.setWavetables(new WaveTable[]{WaveTable.Saw,WaveTable.Saw,WaveTable.Saw});
-        b.setOffsets(new double[]{1d,1.02f, 0.999f});
-        b.setNoteLFO(LFOFactory.sineLFO(2f,1f,1000));
-
-        List<Pair<Float, Long>> notes = new ArrayList<>();
-        notes.add(new Pair<>(100f, 10000l)); // ricordarsi il / 1000 dai micros
-        notes.add(new Pair<>(200f, 50000l)); // ricordarsi il / 1000 dai micros
-
-        try {
-            synthetizers.add(b.build(notes));
-        } catch (Exception e) { e.printStackTrace(); }
-
-
-
     }
-
-    private long pos = 0;
-
     /**
      * Calculates and plays a bufffer to the LibGDX audio device.
      */
     public void playBuffer() {
 
-
-
-        if (pos++ == 100) {
-            synthetizers.get(0).playTimedNote(100f, 10000000l);
-        }
-
-        /*
-        if (pos == 0 || pos == 30  || pos == 60||  pos == 90 || pos == 180 || pos == 195+15) {
-            ds.playPercussion(DrumSamples.Kick);
-        }
-        if (pos == 15 || pos == 15+7 || pos == 60 || pos == 75 || pos == 82 || pos == 120 || pos == 150 || pos == 195 || pos == 195 + 7) {
-            ds.playPercussion(DrumSamples.Hat);
-        }
-        if (pos == 45 || pos == 165 || pos == 225) {
-            ds.playPercussion(DrumSamples.Snare);
-        }
-        if (pos == 105  || pos == 135) {
-            ds.playPercussion(DrumSamples.Tom);
-        }
-*/
-
-        /*
-        if(pos == 15 || pos == 45 || pos == 60 || pos == 52 || pos == 67 || pos == 105){
-            ds.playPercussion(DrumSamples.Hat);
-        }
-        if(pos == 30 || pos == 90){
-            ds.playPercussion(DrumSamples.Snare);
-        }
-         */
-
-
-
         int num = synthetizers.stream().mapToInt(Synth::checkKeys).sum();
-        for (int i = 0; i < buffer.length; i++) {
-            buffer[i] = (float) (synthetizers.stream().mapToDouble(Synth::getSample).sum() * 1);
+        if (old > 0 && old != num) {
+            att = 100;
+            step = ((calc(num)) - vol) / att;
+        } else {
+            vol = calc(num);
         }
 
-/*
-        System.out.println(ds.checkKeys());
-        for(int i=0;i<buffer.length;i++){
-            buffer[i] = ds.getSample();
+        for (int i = 0; i < Settings.BUFFER_LENGHT; i++) {
+            if (att != 0) {
+                vol += step;
+                att--;
+            }
+            buffer[i] = (float) (synthetizers.stream().mapToDouble(Synth::getSample).sum()) * vol;
         }
 
+        ad.writeSamples(buffer, 0, buffer.length);
+        old = num;
 
- */
+    }
+
+    public void addDrum() {
+        synthetizers.add(new DrumSynth());
+    }
+
+    public void addSynth(final MidiTrack track) {
+        SynthBuilderImpl b = new SynthBuilderImpl();
+        b.setEnveloper(new Enveloper(10l, 1f, 100l));
+        b.setWavetables(new WaveTable[]{WaveTable.Sine});
+        b.setOffsets(new double[]{1f});
+
+        List<Pair<Float, Long>> notes = new ArrayList<>();
+        var actualTrack = (TrackImpl)track;
 
 
-        //System.out.println(num);
 
+        final int NUM_A4 = 69;
+        final int NUM_NOTE = 12;
+        final double FREQ_A4 = 440;
 
-        ad.writeSamples(this.buffer, 0, buffer.length);
+        actualTrack.getNotesMaxDuration().forEach((key, value) -> {
+            float frequency = (float) (Math.pow(2, (double) (key - NUM_A4) / NUM_NOTE) * FREQ_A4);
+            notes.add(new Pair<>(frequency, value / 1000));
+        });
+
+        try {
+            synthetizers.add(b.build(notes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public float calc(final int n) {
+        switch (n) {
+            case 1: return 0.3f;
+            case 2: return 0.25f;
+            case 3: return 0.22f;
+            case 4: return 0.16f;
+            case 5: return 0.15f;
+            default: return 1f / (float)n; //0.16f-
+        }
     }
 }
