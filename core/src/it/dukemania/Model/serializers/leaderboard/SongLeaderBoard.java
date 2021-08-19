499007182b7465b0ef73883ac8a29a69bc874840
alebrasi <alessandro.brasini1@gmail.com>
2021-08-19 16:56:51 +0200

    Added SongLeaderBoard and relative serializer and deserializer

---
+++ core/src/it/dukemania/Model/serializers/leaderboard/SongLeaderBoard.java
@@ -0,0 +1,30 @@
+package it.dukemania.Model.serializers.leaderboard;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+import java.util.Map;
+
+@JsonDeserialize(using = SongLeaderBoardDeserializer.class)
+@JsonSerialize(using = SongLeaderBoardSerializer.class)
+public class SongLeaderBoard {
+    private final String songHash;
+    private final Map<String, Integer> usersScore;
+
+    public SongLeaderBoard(final String songHash, final Map<String, Integer> usersScore) {
+        this.songHash = songHash;
+        this.usersScore = usersScore;
+    }
+
+    public String getSongHash() {
+        return this.songHash;
+    }
+
+    public void setUserScore(final String playerName, final Integer score) {
+        usersScore.computeIfPresent(playerName, (p, s) -> s < score ? score : s);
+    }
+
+    public Map<String, Integer> getUsersScore() {
+        return Map.copyOf(usersScore);
+    }
+}
---
+++ core/src/it/dukemania/Model/serializers/leaderboard/SongLeaderBoardDeserializer.java
@@ -0,0 +1,45 @@
+package it.dukemania.Model.serializers.leaderboard;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.ObjectCodec;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import it.dukemania.Model.serializers.song.TrackInfo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class SongLeaderBoardDeserializer extends StdDeserializer<SongLeaderBoard> {
+
+    public SongLeaderBoardDeserializer() {
+        this(null);
+    }
+
+    public SongLeaderBoardDeserializer(final Class<SongLeaderBoard> t) {
+        super(t);
+    }
+
+    @Override
+    public SongLeaderBoard deserialize(final JsonParser p, final DeserializationContext ctxt) throws IOException, JsonProcessingException {
+
+        ObjectCodec codec = p.getCodec();
+        JsonNode node = codec.readTree(p);
+
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode scoresNode = node.get("scores");
+
+        JavaType mapLeaderBoardType = mapper.constructType(new TypeReference<Map<String, Integer>>() {
+        });
+
+        Map<String, Integer> usersScore = mapper.readValue(mapper.treeAsTokens(scoresNode), mapLeaderBoardType);
+
+        return new SongLeaderBoard(node.get("songHash").asText(), usersScore);
+    }
+}
