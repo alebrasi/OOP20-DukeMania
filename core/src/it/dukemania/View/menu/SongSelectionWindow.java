a1ecf453200d936edf09c73b7cfc77db9e57d04a
alebrasi <alessandro.brasini1@gmail.com>
2021-08-19 14:58:42 +0200

    Various fix and code cleanup

--- core/src/it/dukemania/View/menu/SongSelectionWindow.java
+++ core/src/it/dukemania/View/menu/SongSelectionWindow.java
@@ -8,13 +8,17 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.NinePatchDrawable;
+import com.badlogic.gdx.utils.Align;
 import it.dukemania.Controller.filedialog.DialogResult;
 import it.dukemania.Controller.songselection.SongSelectionWindowController;
 import it.dukemania.Controller.songselection.SongSelectionWindowControllerImpl;
+import it.dukemania.Model.serializers.song.SongInfo;
 import it.dukemania.View.AbstractView;
 import it.dukemania.midi.InstrumentType;
 import it.dukemania.windowmanager.DukeManiaWindowState;
 
+import javax.sound.midi.InvalidMidiDataException;
+import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.List;
@@ -22,12 +26,13 @@ import java.util.stream.Collectors;
 
 public class SongSelectionWindow extends AbstractView {
 
-    private final SongSelectionWindowController controller = new SongSelectionWindowControllerImpl();
-    private final int FILE_DIALOG_SIZE = 800;
-    private final int TABLE_CONFIGS_OFFSET_Y = -300;
-    private final int TABLE_PADDING = 30;
-    private final float TABLE_BACKGROUND_OPACITY = 0.2f;
-    private final float TABLE_TRACKS_HEIGHT = 200f;
+    private final SongSelectionWindowController controller = new SongSelectionWindowControllerImpl();;
+    private static final int FILE_DIALOG_SIZE = 800;
+    private static final int TABLE_CONFIGS_OFFSET_Y = -300;
+    private static final int TABLE_PADDING = 30;
+    private static final float TABLE_BACKGROUND_OPACITY = 0.2f;
+    private static final float TABLE_TRACKS_HEIGHT = 200f;
+    private final Texture tableBackgroundTexture = new Texture(Gdx.files.internal("background.png"));
 
     public SongSelectionWindow(final String backgroundPath, final Skin skin) throws NoSuchAlgorithmException {
         super(backgroundPath, skin);
@@ -45,16 +50,18 @@ public class SongSelectionWindow extends AbstractView {
         Table tblTracks = new Table();
 
         TextButton btnSongSelect = new TextButton("Select song", skin);
-        Label lblSongName = new Label("", skin);
+        Label lblFileName = new Label("", skin);
         Label lblConfig = new Label("Configure Song", skin);
         Label lblTrackName = new Label("Track Name", skin);
         Label lblInstruments = new Label("Instrument", skin);
         Label lblChannel = new Label("Channel", skin);
+        Label lblSongName = new Label("", skin);
+        Label lblBPM = new Label("", skin);
+        Label lblDifficultyLevel = new Label("Difficulty", skin);
         TextButton btnBackToTitle = new TextButton("Back to title screen", skin);
         TextButton btnSaveSongConfigs = new TextButton("Save configuration", skin);
         TextButton btnPlayTrack = new TextButton("Play Track", skin);
 
-        //TODO Fix ScrollPane vertical scroll (overflowing from screen)
         ScrollPane scrollTableTracks = new ScrollPane(tblTracks, skin);
         scrollTableTracks.setFadeScrollBars(false);
         scrollTableTracks.setScrollingDisabled(true, false);
@@ -62,31 +69,56 @@ public class SongSelectionWindow extends AbstractView {
         FileDialog fd = new FileDialog("Select song", skin);
         ButtonGroup<CheckBox> playableTracks = new ButtonGroup<>();
 
+        //TODO Fix layout
         fd.setResultListener((res, fileName, filePath) -> {
             if (res.equals(DialogResult.OK)) {
-                lblSongName.setText(fileName);
                 playableTracks.clear();
                 playableTracks.setMinCheckCount(1);
                 playableTracks.setMaxCheckCount(1);
-                controller.openSong(filePath);
-                String[] availableInstruments = controller.getAllInstruments();
+                boolean isACorrectMidiFile = true;
+                try {
+                    controller.openSong(filePath);
+                } catch (InvalidMidiDataException e) {
+                    new ErrorDialog("The selected file is not a valid MIDI", skin).show(mainStage);
+                    e.printStackTrace();
+                    isACorrectMidiFile = false;
+                } catch (IOException e) {
+                    new ErrorDialog("Could not read the selected file", skin).show(mainStage);
+                    e.printStackTrace();
+                    isACorrectMidiFile = false;
+                }
+                if (isACorrectMidiFile) {
+                    String[] availableInstruments = controller.getAllInstruments();
 
-                tblTracks.clearChildren();
-                controller.getTracks().forEach(s -> {
-                    SelectBox<String> slctInstruments = new SelectBox<>(skin);
-                    slctInstruments.setItems(availableInstruments);
-                    slctInstruments.setSelected(s.getInstrument().toString());
-                    slctInstruments.setMaxListCount(3);
-                    CheckBox ck = new CheckBox(String.valueOf(s.getChannel()), skin);
-                    playableTracks.add(ck);
-                    tblTracks.add(ck).uniformX().padLeft(TABLE_PADDING);
-                    TextField txtTrackName = new TextField(s.getTrackName(), skin);
-                    tblTracks.add(txtTrackName).expand().fillX().uniformX().padRight(TABLE_PADDING);
-                    tblTracks.add(slctInstruments).uniformX().right().padRight(TABLE_PADDING);
-                    tblTracks.row();
-                });
-                tblTracks.pack();
-                tblConfigSong.getCell(scrollTableTracks).width(tblTracks.getWidth() + 100);
+                    SongInfo selectedSong = controller.getSongInfo();
+                    lblFileName.setText(fileName);
+                    lblSongName.setText("Song name: " + selectedSong.getTitle());
+                    lblBPM.setText("BPM: " + selectedSong.getBPM());
+                    lblSongName.setAlignment(Align.center);
+                    lblBPM.setAlignment(Align.center);
+
+                    tblTracks.clearChildren();
+
+                    selectedSong.getTracks().forEach(s -> {
+                        SelectBox<String> slctInstruments = new SelectBox<>(skin);
+                        slctInstruments.setItems(availableInstruments);
+                        slctInstruments.setSelected(s.getInstrument().toString());
+                        slctInstruments.setMaxListCount(3);
+                        CheckBox ck = new CheckBox(String.valueOf(s.getChannel()), skin);
+                        playableTracks.add(ck);
+                        tblTracks.add(ck).uniformX().fillX().padLeft(TABLE_PADDING);
+                        TextField txtTrackName = new TextField(s.getTrackName(), skin);
+                        tblTracks.add(txtTrackName).uniformX().fillX().padRight(TABLE_PADDING);
+                        tblTracks.add(new Label(s.getDifficultyLevel().getEffectiveName(), skin)).fillX().uniformX();
+                        tblTracks.add(slctInstruments).fillX().uniformX().right().padRight(TABLE_PADDING);
+                        tblTracks.row();
+                    });
+                    //tblTracks.pack();
+                    //tblConfigSong.getCell(scrollTableTracks).uniformX().fillX();//.width(tblTracks.getWidth() + 100);
+                    /*tblConfigSong.getCell(scrollTableTracks).expandX().fillX().colspan(4);
+                    scrollTableTracks.pack();
+                    tblConfigSong.pack();*/
+                }
             }
         });
 
@@ -101,7 +133,7 @@ public class SongSelectionWindow extends AbstractView {
         btnBackToTitle.addListener(new ClickListener() {
             @Override
             public void clicked(final InputEvent event, final float x, final float y) {
-                switchWindowNotifier.switchWindow(DukeManiaWindowState.TITLE);
+                switchWindowNotifier.switchWindow(DukeManiaWindowState.TITLE, null);
             }
         });
 
@@ -109,42 +141,57 @@ public class SongSelectionWindow extends AbstractView {
             @Override
             public void clicked(final InputEvent event, final float x, final float y) {
                 controller.setPlayTrack(playableTracks.getCheckedIndex());
+                controller.playSong(switchWindowNotifier);
+                //switchWindowNotifier.switchWindow(DukeManiaWindowState.PLAY);
+            }
+        });
+
+        btnSaveSongConfigs.addListener(new ClickListener() {
+            @Override
+            public void clicked(final InputEvent event, final float x, final float y) {
                 List<String> trackNames = Arrays.stream(tblTracks.getChildren().toArray())
                                                             .filter(l -> l.getClass().equals(TextField.class))
                                                             .map(f -> ((TextField) f).getText())
                                                             .collect(Collectors.toList());
+
+                //Disabled unchecked warning for line 128. Is always a SelectBox<String>
+                @SuppressWarnings("unchecked")
                 List<InstrumentType> instruments = Arrays.stream(tblTracks.getChildren().toArray())
                                                             .filter(l -> l.getClass().equals(SelectBox.class))
-                                                            .map(f -> (SelectBox<String>)f)
-                                                            .map(f -> InstrumentType.valueOf(f.getSelected()))
+                                                            .map(f -> ((SelectBox<String>) f).getSelected())
+                                                            .map(InstrumentType::valueOf)
                                                             .collect(Collectors.toList());
                 controller.updateTracks(trackNames, instruments);
-                //switchWindowNotifier.switchWindow(DukeManiaWindowState.PLAY);
             }
         });
 
         //Sets the layout of the song table configuration
         tblConfigSong.add(btnSongSelect);
-        tblConfigSong.add(lblSongName).expand().fillX();
+        tblConfigSong.add(lblFileName).expand().fillX();
         tblConfigSong.row();
-        tblConfigSong.add(lblConfig).colspan(3).expand().center().padBottom(TABLE_PADDING);
+        tblConfigSong.add(lblSongName).expand().fillX().colspan(4).padBottom(TABLE_PADDING);
+        tblConfigSong.row();
+        tblConfigSong.add(lblBPM).expand().fillX().colspan(4);
+        tblConfigSong.row();
+        tblConfigSong.add(lblConfig).colspan(4).expand().center().padBottom(TABLE_PADDING);
         tblConfigSong.row();
         tblConfigSong.add(lblChannel).expandX().uniformX().padLeft(TABLE_PADDING);
         tblConfigSong.add(lblTrackName).expandX().uniformX();
+        tblConfigSong.add(lblDifficultyLevel).expandX().uniformX();
         tblConfigSong.add(lblInstruments).expandX().uniformX().padRight(TABLE_PADDING);
         tblConfigSong.row();
-        tblConfigSong.add(scrollTableTracks).expand().fillX().colspan(3).height(TABLE_TRACKS_HEIGHT);
+        tblConfigSong.add(scrollTableTracks).expandX().fillX().colspan(4).height(TABLE_TRACKS_HEIGHT);
         tblConfigSong.row();
         tblConfigSong.add(btnBackToTitle).expandX().left().padLeft(TABLE_PADDING);
-        tblConfigSong.add(btnSaveSongConfigs).expandX().colspan(2).right().padRight(TABLE_PADDING);
+        tblConfigSong.add(btnSaveSongConfigs).expandX().colspan(3).right().padRight(TABLE_PADDING);
         tblConfigSong.row();
-        tblConfigSong.add(btnPlayTrack).expandX().colspan(3).fillX();
+        tblConfigSong.add(btnPlayTrack).expandX().colspan(4).fillX();
 
         //TODO Dispose texture and rename file
-        NinePatch patch = new NinePatch(new Texture(Gdx.files.internal("background.png")), 0, 0, 0, 0);
-        NinePatchDrawable sas = new NinePatchDrawable(patch);
+        NinePatch patch = new NinePatch(tableBackgroundTexture, 0, 0, 0, 0);
+        NinePatchDrawable drawablePatch = new NinePatchDrawable(patch);
         patch.setColor(new Color(1, 1, 1, TABLE_BACKGROUND_OPACITY));
-        tblConfigSong.background(sas);
+        tblConfigSong.background(drawablePatch);
 
         mainMenuContainer.setActor(tblConfigSong);
         mainMenuContainer.setPosition(screenWidth / 2, (screenHeight / 2) + TABLE_CONFIGS_OFFSET_Y);
