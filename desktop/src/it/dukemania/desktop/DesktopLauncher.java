a273e643721706a90820e38d0986ece60d32eb6a
alebrasi <alessandro.brasini1@gmail.com>
2021-08-20 16:17:04 +0200

    Changed class entry point

--- desktop/src/it/dukemania/desktop/DesktopLauncher.java
+++ desktop/src/it/dukemania/desktop/DesktopLauncher.java
@@ -1,8 +1,10 @@
 package it.dukemania.desktop;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
+import it.dukemania.DukeManiaTest;
 import it.dukemania.PlayScreen;
 import it.dukemania.View.notesGraphics.Size;
 import it.dukemania.View.notesGraphics.SizeImpl;
@@ -10,9 +12,6 @@ import it.dukemania.View.notesGraphics.SizeImpl;
 public class DesktopLauncher {
     public static void main(final String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-        Size dimensions = new SizeImpl();
-        config.setWindowedMode(dimensions.getSize().getX(), dimensions.getSize().getY());
-        config.setResizable(false);
-        new Lwjgl3Application(new PlayScreen(), config);
+        new Lwjgl3Application(new DukeManiaTest(), config);
     }
 }
