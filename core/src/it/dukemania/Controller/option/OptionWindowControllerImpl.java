f20279296eae4c9792b14e72ecb841289060bb08
alebrasi <alessandro.brasini1@gmail.com>
2021-08-22 20:50:34 +0200

    Implemented controller for OptionWindow

--- core/src/it/dukemania/Controller/option/OptionWindowControllerImpl.java
+++ core/src/it/dukemania/Controller/option/OptionWindowControllerImpl.java
@@ -1,4 +1,22 @@
 package it.dukemania.Controller.option;
 
-public class OptionWindowControllerImpl {
+import it.dukemania.Model.GameModel;
+
+public class OptionWindowControllerImpl implements OptionWindowController {
+
+    private final GameModel data;
+
+    public OptionWindowControllerImpl(final GameModel data) {
+        this.data = data;
+    }
+
+    @Override
+    public void setPlayerName(final String name) {
+        data.setPlayerName(name);
+    }
+
+    @Override
+    public String getPlayerName() {
+        return data.getPlayerName();
+    }
 }
--- core/src/it/dukemania/View/menu/OptionWindow.java
+++ core/src/it/dukemania/View/menu/OptionWindow.java
@@ -4,24 +4,17 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JavaType;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import it.dukemania.Model.serializers.ConfigurationsModel;
-import it.dukemania.Model.serializers.ConfigurationsModelImpl;
-import it.dukemania.Model.serializers.leaderboard.SongLeaderBoard;
+import it.dukemania.Controller.option.OptionWindowController;
+import it.dukemania.Controller.option.OptionWindowControllerImpl;
+import it.dukemania.Model.GameModel;
 import it.dukemania.View.AbstractView;
-import it.dukemania.midi.Song;
-import it.dukemania.util.storage.Storage;
-import it.dukemania.util.storage.StorageFactoryImpl;
 import it.dukemania.windowmanager.DukeManiaWindowState;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
 
 public class OptionWindow extends AbstractView {
+
+    private OptionWindowController controller;
+
     public OptionWindow(final String backgroundPath, final Skin skin) {
         super(backgroundPath, skin);
     }
@@ -29,10 +22,13 @@ public class OptionWindow extends AbstractView {
     @Override
     public void create() {
         super.create();
+
+        controller = new OptionWindowControllerImpl(data);
+
         float screenWidth = mainStage.getWidth();
         float screenHeight = mainStage.getHeight();
         Label lblPlayerName = new Label("Player name: ", skin);
-        TextField txtPlayerName = new TextField("P1", skin);
+        TextField txtPlayerName = new TextField(controller.getPlayerName(), skin);
         TextButton btnSave = new TextButton("Save name", skin);
         TextButton btnBack = new TextButton("Back to title screen", skin);
 
@@ -46,16 +42,13 @@ public class OptionWindow extends AbstractView {
         btnSave.addListener(new ClickListener() {
            @Override
            public void clicked(final InputEvent event, final float x, final float y) {
-               data.setPlayerName(txtPlayerName.getText());
-               /*
-               Map<String, Integer> scores = Map.of("Pinco", -420, "Pallino", -69, "Topolino", 69420);
-               List<SongLeaderBoard> l = List.of(new SongLeaderBoard("dffsfdsf", scores), new SongLeaderBoard("kjshfdjkhsjkfhsf", scores));
-                */
+               controller.setPlayerName(txtPlayerName.getText());
            }
         });
 
         Table table = new Table(skin);
 
+        //TODO Remove magic numbers
         table.add(lblPlayerName);
         table.add(txtPlayerName);
         table.row().padBottom(20);
