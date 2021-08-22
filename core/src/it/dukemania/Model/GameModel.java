c08914f5576cab938af8fa93e49564fd8472b11f
alebrasi <alessandro.brasini1@gmail.com>
2021-08-22 18:55:32 +0200

    Added GameModel class

---
+++ core/src/it/dukemania/Model/GameModel.java
@@ -0,0 +1,63 @@
+package it.dukemania.Model;
+
+import it.dukemania.midi.MidiTrack;
+import it.dukemania.midi.Song;
+
+public class GameModel {
+    private String songHash;
+    private String playerName = "P1";
+    private Song selectedSong;
+    private MidiTrack selectedTrack;
+
+    public MidiTrack getSelectedTrack() {
+        return selectedTrack;
+    }
+
+    public void setSelectedTrack(final MidiTrack selectedTrack) {
+        this.selectedTrack = selectedTrack;
+    }
+
+    public int getNumColumns() {
+        return numColumns;
+    }
+
+    public void setNumColumns(final int numColumns) {
+        this.numColumns = numColumns;
+    }
+
+    private int numColumns;
+    private int score;
+
+    public void setSelectedSong(final Song selectedSong) {
+        this.selectedSong = selectedSong;
+    }
+
+    public Song getSelectedSong() {
+        return selectedSong;
+    }
+
+    public void setSongHash(final String songHash) {
+        this.songHash = songHash;
+    }
+
+    public void setPlayerName(final String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setScore(final int score) {
+        this.score = score;
+    }
+
+    public String getSongHash() {
+        return songHash;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+}
