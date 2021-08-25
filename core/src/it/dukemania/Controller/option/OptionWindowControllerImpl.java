80e0370ed00954737b044b5afefb5a11187c2756
alebrasi <alessandro.brasini1@gmail.com>
2021-08-25 19:39:33 +0200

    Added sample rate and buffer size selection

--- core/src/it/dukemania/Controller/option/OptionWindowControllerImpl.java
+++ core/src/it/dukemania/Controller/option/OptionWindowControllerImpl.java
@@ -1,6 +1,7 @@
 package it.dukemania.Controller.option;
 
 import it.dukemania.Model.GameModel;
+import it.dukemania.audioengine.Settings;
 
 public class OptionWindowControllerImpl implements OptionWindowController {
 
@@ -18,5 +19,35 @@ public class OptionWindowControllerImpl implements OptionWindowController {
     @Override
     public final String getPlayerName() {
         return data.getPlayerName();
+    }
+
+    @Override
+    public final Integer[] getAvailableSampleRates() {
+        return Settings.AVAILABLE_SAMPLE_RATES;
+    }
+
+    @Override
+    public final Integer[] getBufferSizes() {
+        return Settings.AVAILABLE_BUFFER_SIZES;
+    }
+
+    @Override
+    public final void setSampleRate(final int sampleRate) {
+        Settings.SAMPLE_RATE = sampleRate;
+    }
+
+    @Override
+    public final void setBufferSize(final int bufferSize) {
+        Settings.BUFFER_LENGHT = bufferSize;
+    }
+
+    @Override
+    public final int getCurrentSampleRate() {
+        return (int) Settings.SAMPLE_RATE;
+    }
+
+    @Override
+    public final int getCurrentBufferSize() {
+        return Settings.BUFFER_LENGHT;
     }
 }
--- core/src/it/dukemania/View/menu/OptionWindow.java
+++ core/src/it/dukemania/View/menu/OptionWindow.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.SelectBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -24,7 +25,7 @@ public class OptionWindow extends AbstractView {
     }
 
     @Override
-    public void create() {
+    public final void create() {
         super.create();
 
         controller = new OptionWindowControllerImpl(data);
@@ -32,30 +33,46 @@ public class OptionWindow extends AbstractView {
         float screenWidth = mainStage.getWidth();
         float screenHeight = mainStage.getHeight();
         Label lblPlayerName = new Label("Player name: ", skin);
+        Label lblSampleRate = new Label("Sample rate: ", skin);
+        Label lblBufferSize = new Label("Audio buffer size", skin);
         TextField txtPlayerName = new TextField(controller.getPlayerName(), skin);
         TextButton btnSave = new TextButton("Save name", skin);
         TextButton btnBack = new TextButton("Back to title screen", skin);
+        SelectBox<Integer> slctSampleRates = new SelectBox<>(skin);
+        SelectBox<Integer> slctBufferSizes = new SelectBox<>(skin);
+
+        slctSampleRates.setItems(controller.getAvailableSampleRates());
+        slctBufferSizes.setItems(controller.getBufferSizes());
+        slctSampleRates.setSelected(controller.getCurrentSampleRate());
+        slctBufferSizes.setSelected(controller.getCurrentBufferSize());
 
         btnBack.addListener(new ClickListener() {
-           @Override
-           public void clicked(final InputEvent event, final float x, final float y) {
-               switchWindowNotifier.switchWindow(DukeManiaWindowState.TITLE, data);
-           }
+            @Override
+            public void clicked(final InputEvent event, final float x, final float y) {
+                switchWindowNotifier.switchWindow(DukeManiaWindowState.TITLE, data);
+            }
         });
 
         btnSave.addListener(new ClickListener() {
-           @Override
-           public void clicked(final InputEvent event, final float x, final float y) {
-               controller.setPlayerName(txtPlayerName.getText());
-           }
+            @Override
+            public void clicked(final InputEvent event, final float x, final float y) {
+                controller.setPlayerName(txtPlayerName.getText());
+                controller.setBufferSize(slctBufferSizes.getSelected());
+                controller.setSampleRate(slctSampleRates.getSelected());
+            }
         });
 
         Table table = new Table(skin);
 
-        //TODO Remove magic numbers
         table.add(lblPlayerName);
         table.add(txtPlayerName);
-        table.row().padBottom(20);
+        table.row();
+        table.add(lblSampleRate);
+        table.add(slctSampleRates);
+        table.row();
+        table.add(lblBufferSize);
+        table.add(slctBufferSizes);
+        table.row();
         table.add(btnBack).colspan(2).expand().left();
         table.add(btnSave).colspan(2).expand().right();
 
